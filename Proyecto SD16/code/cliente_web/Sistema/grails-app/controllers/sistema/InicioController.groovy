fed834651c4fd811e103f98949e0cffda73d705b
Tania Monges <taniamonges@gmail.com>
2016-11-15 21:05:47 -0300

    se modifica iniciocontroller para mostrar en acceso directo la cantidad de fichas con estado recibido, en vez de todas

--- Proyecto SD16/code/cliente_web/Sistema/grails-app/controllers/sistema/InicioController.groovy
+++ Proyecto SD16/code/cliente_web/Sistema/grails-app/controllers/sistema/InicioController.groovy
@@ -4,6 +4,7 @@ import com.sd.uni.labpatologia.service.article.IArticleService
 import com.sd.uni.labpatologia.service.laboratory.ILaboratoryService;
 import com.sd.uni.labpatologia.service.patient.IPatientService
 import com.sd.uni.labpatologia.service.request.IRequestService
+import com.sd.uni.labpatologia.util.StatusEnum;
 
 import grails.gsp.PageRenderer
 
@@ -13,10 +14,13 @@ class InicioController {
 	def IArticleService articleService
 	def IRequestService requestService
     def index() { 
+		String text = "status=RECIBIDO"
+		
+		def cantRecibido = requestService.find(text,0,0)
 		[laboratoryInstanceList:laboratoryService.getAll(),
 			patientInstanceList:patientService.getAll(),
 			articleInstanceList:articleService.getAll(),
-			requestInstanceList:requestService.getAll()]
+			requestInstanceList:cantRecibido]
 	}
 	
 }
